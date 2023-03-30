d3b4bd33440d7b92cd16b4ddd8b7e80155cf1b51
jasojone <jasojone@gmail.com>
2023-03-30 14:07:21 -0700

    moves utils

--- star-match/src/App.js
+++ star-match/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
+import utils from './utils/math-utils';
 
 // STAR MATCH - V9
 const mountNode = document.getElementById('root');
@@ -144,35 +145,35 @@ const colors = {
   candidate: 'deepskyblue',
 };
 
-// Math science
-const utils = {
-  // Sum an array
-  sum: arr => arr.reduce((acc, curr) => acc + curr, 0),
+// // Math science
+// const utils = {
+//   // Sum an array
+//   sum: arr => arr.reduce((acc, curr) => acc + curr, 0),
 
-  // create an array of numbers between min and max (edges included)
-  range: (min, max) => Array.from({length: max - min + 1}, (_, i) => min + i),
+//   // create an array of numbers between min and max (edges included)
+//   range: (min, max) => Array.from({length: max - min + 1}, (_, i) => min + i),
 
-  // pick a random number between min and max (edges included)
-  random: (min, max) => min + Math.floor(Math.random() * (max - min + 1)),
+//   // pick a random number between min and max (edges included)
+//   random: (min, max) => min + Math.floor(Math.random() * (max - min + 1)),
 
-  // Given an array of numbers and a max...
-  // Pick a random sum (< max) from the set of all available sums in arr
-  randomSumIn: (arr, max) => {
-    const sets = [[]];
-    const sums = [];
-    for (let i = 0; i < arr.length; i++) {
-      for (let j = 0, len = sets.length; j < len; j++) {
-        const candidateSet = sets[j].concat(arr[i]);
-        const candidateSum = utils.sum(candidateSet);
-        if (candidateSum <= max) {
-          sets.push(candidateSet);
-          sums.push(candidateSum);
-        }
-      }
-    }
-    return sums[utils.random(0, sums.length - 1)];
-  },
-};
+//   // Given an array of numbers and a max...
+//   // Pick a random sum (< max) from the set of all available sums in arr
+//   randomSumIn: (arr, max) => {
+//     const sets = [[]];
+//     const sums = [];
+//     for (let i = 0; i < arr.length; i++) {
+//       for (let j = 0, len = sets.length; j < len; j++) {
+//         const candidateSet = sets[j].concat(arr[i]);
+//         const candidateSum = utils.sum(candidateSet);
+//         if (candidateSum <= max) {
+//           sets.push(candidateSet);
+//           sums.push(candidateSum);
+//         }
+//       }
+//     }
+//     return sums[utils.random(0, sums.length - 1)];
+//   },
+// };
 export default StarMatch;
 // ReactDOM.render(<StarMatch />, mountNode);	
 
